a1781fb862310bc2a582e86a99d90a72e732a1dd
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-06-22 11:12:51 +0300

    Init project

---
+++ db/server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+const { DB_HOST } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(8000, () => {
+      console.log("Server running. Use our API on port: 8000");
+    });
+  })
+  .catch((error) => {
+    console.log("error.message", error.message);
+
+    process.exit(1);
+  });
